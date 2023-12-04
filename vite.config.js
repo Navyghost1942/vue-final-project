0efb1b767f05a229a20fa908ace4c8aba5743d43
NavyGhost1942 <riverawilliam256@gmail.com>
2023-12-03 21:36:13 -0600

    fixed name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-company-directory/',
+  base: '/vue-final-project/',
   plugins: [vue()],
   resolve: {
     alias: {
